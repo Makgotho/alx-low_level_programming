a7fb1faea3366edae71048ca6fb5373a8a8ae705
Makgotho <mphmolepo5@gmail.com>
2023-09-28 18:04:42 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,7 +13,7 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_palindrome(s, 0, _strlen_recursion(s)));
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
@@ -30,19 +30,19 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * check_palindrome - checks the characters recursively for palindrome
+ * check_pal - checks the characters recursively for palindrome
  * @s: string to check
  * @i: iterator
  * @len: length of the string
  *
  * Return: 1 if palindrome, 0 if not
  */
-int check_palindrome(char *s, int i, int len)
+int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
 		return (0);
 	if (i >= len)
 		return (1);
-	return (check_palindrome(s, i + 1, len - 1));
+	return (check_pal(s, i + 1, len - 1));
 }
 
